3a1ef54988bf83c0956c5b684c6b68819446d94c
HannaPo <114150796+HannaPo@users.noreply.github.com>
2023-06-11 20:23:28 +0300

    redux

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -11,6 +11,10 @@ const ContactList = () => {
 
   const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
 
+//   const filteredContacts = contacts.filter(contact =>
+//   contact.name?.toLowerCase().includes(filter?.toLowerCase())
+// );
+  
   return (
     <Wrapper>
       {filteredContacts.map(({ id, name, number }) => (
